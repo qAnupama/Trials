b6b9ef380ba4e02ab219897f798aa0aa22882965
qAnupama <121783793+qAnupama@users.noreply.github.com>
2023-01-18 11:29:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='Trials',
-      version='0.83',
+      version='0.84',
       scripts=['__init__.py'],
-      packages=['Trials],
+      packages=['Trials'],
       url='https://github.com/qAnupama/Trials.git',
       license='MIT',
       packages=['Trials']
