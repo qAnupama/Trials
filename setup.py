0bd722e343ad88d8d94d48d248f004f46330e84a
qAnupama <121783793+qAnupama@users.noreply.github.com>
2023-01-18 11:22:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Trials',
-      version='0.81',
+      version='0.82',
       scripts=['__init__.py'],
       packages=find_packages()
       )
